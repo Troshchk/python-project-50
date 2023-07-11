f2a780498d0b73f4f304d188ea155708943d5049
Troshchk <ktroshchenkova@ripe.net>
2023-07-11 20:48:46 +0200

    Fix line length according to the flake8 rules

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,8 @@ import json
 def main():
     parser = argparse.ArgumentParser(
                     prog='gendiff',
-                    description='Compares two configuration files and shows a difference.')
+                    description='''Compares two configuration files and shows
+                                a difference.''')
     parser.add_argument('first_file')
     parser.add_argument('second_file')
     parser.add_argument('--format', '-f', help="set format of output")
