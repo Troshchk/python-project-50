d439f733ab5868dd0189b84dde2946f7deacbaa0
Troshchk <ktroshchenkova@ripe.net>
2023-07-13 14:24:26 +0200

    Add tests for yaml: tests are meant to fail

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,14 +1,25 @@
 import gendiff.scripts.gendiff as gendiff
 import pytest
 
+
 @pytest.fixture
-def get_file1_path():
+def get_file1_json_path():
     return "./tests/fixtures/file1.json"
 
 
 @pytest.fixture
-def get_file2_path():
+def get_file2_json_path():
     return "./tests/fixtures/file2.json"
+
+
+@pytest.fixture
+def get_file1_yaml_path():
+    return "./tests/fixtures/file1.yaml"
+
+
+@pytest.fixture
+def get_file2_yaml_path():
+    return "./tests/fixtures/file2.yaml"
 
 
 @pytest.fixture
@@ -21,6 +32,16 @@ def get_res_diff():
     return test_output
 
 
-def test_simple_case(get_file1_path, get_file2_path, get_res_diff):
-    out = gendiff.generate_diff(get_file1_path, get_file2_path)
+def test_simple_case_jsons(get_file1_json_path, get_file2_json_path, get_res_diff):
+    out = gendiff.generate_diff(get_file1_json_path, get_file2_json_path)
+    assert out == get_res_diff
+
+
+def test_simple_case_yamls(get_file1_yaml_path, get_file2_yaml_path, get_res_diff):
+    out = gendiff.generate_diff(get_file1_yaml_path, get_file2_yaml_path)
+    assert out == get_res_diff
+
+
+def test_simple_case_json_and_yamls(get_file1_json_path, get_file2_yaml_path, get_res_diff):
+    out = gendiff.generate_diff(get_file1_json_path, get_file2_yaml_path)
     assert out == get_res_diff
