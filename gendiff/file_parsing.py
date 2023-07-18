9e9658576290f73196c0f541c66c3c9a9e4d8227
Troshchk <ktroshchenkova@ripe.net>
2023-07-18 16:34:01 +0200

    Add plain formatter

--- gendiff/file_parsing.py
+++ gendiff/file_parsing.py
@@ -5,7 +5,7 @@ import yaml
 NEW_LEVEL = "    "
 PATTERN = "{}{}{}: {}\n"
 PATTERN2 = "{}{}{}: {}"
-
+PATTERN_PLAIN = "Property '{}' was {}\n"
 
 def jsonify(v):
     if isinstance(v, bool):
@@ -52,6 +52,28 @@ def compare_values(v0, v1):
     return comparison_result, vals
 
 
+def format_string(v):
+    if isinstance(v, str):
+        return f"'{v}'"
+    if isinstance(v, dict):
+        return "[complex value]"
+    if isinstance(v, bool) or v is None:
+        return jsonify(v)
+    return v
+
+
+def compare_values_for_plain(v0, v1):
+    if v0 == v1:
+        comparison_result = None
+    elif isinstance(v0, NotSet):
+        comparison_result = f"added with value: {format_string(v1)}"
+    elif isinstance(v1, NotSet):
+        comparison_result = "removed"
+    else:
+        comparison_result = f"updated. From {format_string(v0)} to {format_string(v1)}"
+    return comparison_result
+
+
 def stylish(diff_dict, indent="  ", out_str=""):
     out_str += "{\n"
     for k, v_in in diff_dict.items():
@@ -69,6 +91,21 @@ def stylish(diff_dict, indent="  ", out_str=""):
             out_str += PATTERN2.format(indent, comparison_result, k, value)
     out_str += f"{indent.replace('  ', '', 1)}"
     out_str += "}\n"
+    return out_str
+
+
+def plain(diff_dict, key=''):
+    out_str = ""
+    for k, v_in in diff_dict.items():
+        if not isinstance(v_in, dict):
+            v0, v1 = v_in
+            #v0, v1 = [v if isinstance(v, dict) else jsonify(v) for v in v_in]
+            comparison_result = compare_values_for_plain(v0, v1)
+            if comparison_result is not None:
+                out_str += PATTERN_PLAIN.format(f"{key}.{k}".lstrip("."), comparison_result)
+        else:
+            comparison_result = plain(v_in, key=f"{key}.{k}")
+            out_str += comparison_result
     return out_str
 
 
@@ -101,4 +138,6 @@ def generate_diff(file1, file2, format=None):
     diff_dict = compare_data(f1, f2)
     if format in [None, "stylish"]:
         out_str = stylish(diff_dict)
+    elif format == "plain":
+        out_str = plain(diff_dict)
     return out_str.rstrip()
