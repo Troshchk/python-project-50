54397ad21bb94831d87db49b524b4126bdcaa67e
Troshchk <ktroshchenkova@ripe.net>
2023-07-18 14:39:33 +0200

    Add tests for the nested inputs

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -13,6 +13,16 @@ def get_file2_json_path():
 
 
 @pytest.fixture
+def get_file1_nested_json_path():
+    return "./tests/fixtures/file1_nested.json"
+
+
+@pytest.fixture
+def get_file2_nested_json_path():
+    return "./tests/fixtures/file2_nested.json"
+
+
+@pytest.fixture
 def get_file1_yaml_path():
     return "./tests/fixtures/file1.yaml"
 
@@ -26,6 +36,15 @@ def get_file2_yaml_path():
 def get_res_diff():
     f = open(
         "./tests/fixtures/test_output1",
+        "r")
+    test_output = f.read()
+    f.close()
+    return test_output
+
+@pytest.fixture
+def get_res_diff_nested():
+    f = open(
+        "./tests/fixtures/output_nested",
         "r")
     test_output = f.read()
     f.close()
@@ -45,3 +64,8 @@ def test_simple_case_yamls(get_file1_yaml_path, get_file2_yaml_path, get_res_dif
 def test_simple_case_json_and_yamls(get_file1_json_path, get_file2_yaml_path, get_res_diff):
     out = gendiff.generate_diff(get_file1_json_path, get_file2_yaml_path)
     assert out == get_res_diff
+
+
+def test_nested_case_jsons(get_file1_nested_json_path, get_file2_nested_json_path, get_res_diff_nested):
+    out = gendiff.generate_diff(get_file1_nested_json_path, get_file2_nested_json_path)
+    assert out == get_res_diff_nested
