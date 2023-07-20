daaf7bd6b057c2363a183980e0bf337ef5f1756c
Troshchk <ktroshchenkova@ripe.net>
2023-07-20 14:15:23 +0200

    Add print for troubleshooting

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -45,6 +45,7 @@ def generate_diff(file1, file2, format=None):
     diff_dict = compare_data(f1, f2)
     if format in [None, "stylish"]:
         out_str = stylish(diff_dict).rstrip("\n")
+        print(out_str)
     elif format == "plain":
         out_str = plain(diff_dict).rstrip("\n")
     elif format == "json":
