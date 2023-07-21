f0a9d3533445629046a6b4c2f89f3a6fa8ad1601
Troshchk <ktroshchenkova@ripe.net>
2023-07-21 12:28:38 +0200

    Simplify structure of the data comparison, output not influenced

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -13,28 +13,24 @@ def load_file(file_path):
         return yaml.safe_load(open(file_path))
 
 
-def compare_data(data1, data2, key_above=None):
+def compare_data(data1, data2):
     """Output contains values and info whether the value was set in the input.
     This is necessary to distinguish None from input and values,
     which are not set"""
     diff_dict = {}
-    if isinstance(data1, dict):
-        if isinstance(data2, dict):
-            for k1, v1 in data1.items():
-                if k1 in data2.keys():
-                    if isinstance(data2[k1], dict):
-                        diff_dict[k1] = compare_data(data1[k1], data2[k1],
-                                                     key_above=k1)
-                    else:
-                        diff_dict[k1] = (v1, data2[k1], True, True)
+    if isinstance(data1, dict) and isinstance(data2, dict):
+        for k1, v1 in data1.items():
+            if k1 in data2.keys():
+                if isinstance(data2[k1], dict):
+                    diff_dict[k1] = compare_data(data1[k1], data2[k1])
                 else:
-                    diff_dict[k1] = (v1, None, True, False)
-            for k2, v2 in data2.items():
-                if k2 not in diff_dict.keys():
-                    diff_dict[k2] = (None, v2, False, True)
-        else:
-            diff_dict[k1] = (data1, data2, True, True)
-    elif isinstance(data2, dict):
+                    diff_dict[k1] = (v1, data2[k1], True, True)
+            else:
+                diff_dict[k1] = (v1, None, True, False)
+        for k2, v2 in data2.items():
+            if k2 not in diff_dict.keys():
+                diff_dict[k2] = (None, v2, False, True)
+    else:
         return (data1, data2, True, True)
     return dict(sorted(diff_dict.items()))
 
