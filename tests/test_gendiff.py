b016e6f5297ff91087da5cb4750891e5cf5ad09d
Troshchk <ktroshchenkova@ripe.net>
2023-08-28 17:48:26 +0200

    Remove tests for plain files

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -13,33 +13,21 @@ def get_test_output(output_file):
     return test_output
 
 
-FILE1_JSON = get_path_to_file("file1.json")
-FILE2_JSON = get_path_to_file("file2.json")
-FILE3_JSON = get_path_to_file("file3.json")
-FILE4_JSON = get_path_to_file("file4.json")
-FILE1_YAML = get_path_to_file("file1.yaml")
-FILE2_YAML = get_path_to_file("file2.yaml")
 FILE1_JSON_NESTED = get_path_to_file("file1_nested.json")
 FILE2_JSON_NESTED = get_path_to_file("file2_nested.json")
 FILE1_YAML_NESTED = get_path_to_file("file1_nested.yaml")
 FILE2_YAML_NESTED = get_path_to_file("file2_nested.yaml")
-OUTPUT_FLAT = get_path_to_file("output_flat")
 OUTPUT_NESTED = get_path_to_file("output_nested")
 OUTPUT_NESTED_PLAIN = get_path_to_file("output_nested_plain")
 OUTPUT_NESTED_JSON = get_path_to_file("output_nested_json")
-OUTPUT_HEXLET_TESTS = get_path_to_file("output_hexlet_tests")
 
 
 @pytest.mark.parametrize(
     "file1,file2,expected",
     [
-        (FILE1_JSON, FILE2_JSON, OUTPUT_FLAT),
-        (FILE1_YAML, FILE2_YAML, OUTPUT_FLAT),
-        (FILE1_JSON, FILE2_YAML, OUTPUT_FLAT),
         (FILE1_JSON_NESTED, FILE2_JSON_NESTED, OUTPUT_NESTED),
         (FILE1_YAML_NESTED, FILE2_YAML_NESTED, OUTPUT_NESTED),
         (FILE1_JSON_NESTED, FILE2_YAML_NESTED, OUTPUT_NESTED),
-        (FILE3_JSON, FILE4_JSON, OUTPUT_HEXLET_TESTS),
     ],
 )
 def test_standard_format(file1, file2, expected):
