1ff1a2efb104d9a2f4be62e761aca4fe3dd72929
Troshchk <ktroshchenkova@ripe.net>
2023-08-28 17:28:24 +0200

    Choose parser based on the extension

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,13 +1,13 @@
 from .comparer import compare_data
 from .formatters.stylish_formatter import stylish
-from .formatters.plain_formatter import plain
-from .formatters.json_fomatter import json_parser
-from .parsers import get_extension, LOADERS
+from .parsers import PARSERS
+import pathlib
+
 
 
 def generate_diff(file1_path, file2_path, format="stylish"):
     file1_data, file2_data = map(
-        lambda file: LOADERS.get(get_extension(file))(file),
+        lambda file: PARSERS.get(pathlib.Path(file).suffix)(file),
         [file1_path, file2_path],
     )
     diff_dict = compare_data(file1_data, file2_data)
--- gendiff/parsers.py
+++ gendiff/parsers.py
@@ -10,8 +10,4 @@ def load_yaml(file_path):
     return yaml.safe_load(open(file_path))
 
 
-def get_extension(file_path):
-    return file_path.split(".")[-1].strip()
-
-
-LOADERS = {"json": load_json, "yaml": load_yaml, "yml": load_yaml}
+PARSERS = {".json": load_json, ".yaml": load_yaml, ".yml": load_yaml}
